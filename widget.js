02ea5eb2ed8127f3997f4719299585d03d24c7e0
lemonbanan4 <lemonhberg@gmail.com>
2025-12-17 01:40:32 +0100

    Initial frontend

---
+++ widget.js
@@ -0,0 +1,7 @@
+console.log("Dental bot widget loaded");
+
+const api = document.currentScript.dataset.api;
+const clinic = document.currentScript.dataset.clinic;
+
+console.log("API:", api);
+console.log("Clinic:", clinic);
